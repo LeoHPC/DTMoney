00611d5f022f4742196401b51423b3cbf3f71fed
Leonardo Henrique Petreca Costa <leopetrecca@gmail.com>
2022-04-18 15:27:49 -0300

    first commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+import logoImg from '../../assets/logo.svg';
+import { Container, Content } from './styles';
+
+interface ModalProps {
+  onOpenNewTransactionModal: () => void;
+}
+
+export function Header({ onOpenNewTransactionModal }: ModalProps) {
+
+  return (
+    <Container>
+      <Content>
+        <img src={logoImg} alt="dt money" />
+        <button type="button" onClick={onOpenNewTransactionModal}>
+          Nova transação
+        </button>
+
+      </Content>
+    </Container>
+  )
+}
